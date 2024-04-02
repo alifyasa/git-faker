8fb65e58db09fae45ae35c16f042bd40b79ce81b
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-04-02 08:53:56 +0700

    feat: main function

--- git_commit_sampler/main.py
+++ git_commit_sampler/main.py
@@ -1,5 +1,25 @@
+from datetime import datetime, timedelta
+
+from git_commit_sampler.simulation import simulate, generate_report
+from git_commit_sampler.create import create_simulated_repository, generate_random_string
+
 def main():
-    pass
+    start_time = datetime.now()
+    end_time = start_time + timedelta(days=10)
+
+    sim_result_path = f"output/simulation/{start_time.date()} to {end_time.date()}.txt"
+
+    sim_result = simulate(start_time, end_time)
+    generate_report(
+        sim_result, start_time, end_time,
+        write_to_file=sim_result_path
+    )
+
+    create_simulated_repository(
+        sim_result_path,
+        f"../shadow-git-{generate_random_string(8)}/"
+    )
+
 
 if __name__ == "__main__":
     main()
